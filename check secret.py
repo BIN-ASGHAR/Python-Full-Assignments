2dcc629e4caaae52c9b3430a61b4582819e319fc
Muhammad Bin Asghar <139099833+BIN-ASGHAR@users.noreply.github.com>
2025-11-15 23:25:36 +0500

    Password Game

---
+++ check secret.py
@@ -0,0 +1,18 @@
+secret = "Ahmad"
+guess = ""
+guess_count = 0     
+guess_limit = 3
+out_of_guesses = False 
+
+while guess != secret and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input(" Enter Your guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+        print(" Oh So SAD you lost, Try Again ")
+else:
+        print(" Congrats You Won! ")
+
+    
